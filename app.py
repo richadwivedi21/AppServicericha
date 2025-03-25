244324298922622cb75ad1f5caa243d8e8abe8a7
Aditya Sharma <devganaditya@gmail.com>
2025-03-25 12:27:17 +0530

    updated connection string for application insights.

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ tracer = trace.get_tracer(__name__)
 
 # Configure Azure Monitor exporter
 exporter = AzureMonitorTraceExporter(
-    connection_string="InstrumentationKey=c8e43d86-8f8f-471a-85dc-6190346ee095"
+    connection_string="InstrumentationKey=c8e43d86-8f8f-471a-85dc-6190346ee095;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralindia.livediagnostics.monitor.azure.com/;ApplicationId=0d62543e-6d26-4250-af89-121fb76a2042"
 )
 span_processor = BatchSpanProcessor(exporter)
 trace.get_tracer_provider().add_span_processor(span_processor)
